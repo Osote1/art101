fad9d34254538010bf0d127f20fd502f97ae1d5b
Osote1 <angelarangel24@outlook.com>
2022-10-17 17:14:02 -0700

    Worked on some decorations plus Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Author: Angela Rangel
+ * Created: 17 October
+ * Licesnse: Public Domain
+ */
+
+ // Define Variable
+ make = "Chevy";
+ model = "Malibu";
+ color = "Pink";
+ year = 2014;
+
+ // calculate
+ age = 2022 - year;
+
+ // output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " years<br>");
